3f134fa6ff5bfc8893e47f4f5c5e4de972a3de12
Douglas <dglswender@gmail.com>
2019-07-16 14:09:15 -0300

    Updates

---
+++ precache-manifest.8da778cba832596b69bcf88a75daaf07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c07f5a36e42b34c8f0665cf01582323",
+    "url": "/douglaswender/alugacar/index.html"
+  },
+  {
+    "revision": "a368e07d9cd029abd097",
+    "url": "/douglaswender/alugacar/static/js/2.1938f4d0.chunk.js"
+  },
+  {
+    "revision": "ee882bbb989d208aa51c",
+    "url": "/douglaswender/alugacar/static/js/main.a6dea099.chunk.js"
+  },
+  {
+    "revision": "adb6dad3ef119890b68c",
+    "url": "/douglaswender/alugacar/static/js/runtime~main.42c09f3d.js"
+  }
+]);
